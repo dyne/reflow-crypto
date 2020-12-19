3ec1f101a45e5a41071363bff7adff7bb200ac75
Jaromil <jaromil@dyne.org>
2020-12-19 14:03:32 +0100

    POC new iteration with improvements

--- multidarkroom.lua
+++ multidarkroom.lua
@@ -29,7 +29,10 @@ print''
 
 msg = str("This is the authenticated message")
 
--- setup
+---------
+-- SETUP
+---------
+
 G1 = ECP.generator()
 G2 = ECP2.generator()
 
@@ -38,99 +41,73 @@ ZK = require_once('crypto_abc')
 issuer = ZK.issuer_keygen()
 
 -- keygen: δ = r.O ; γ = δ.G2
-sk1 = INT.random()
-ck1 = INT.random()
-pk1 = G2 * sk1
+sk1 = INT.random() -- signing key
+ck1 = INT.random() -- credential key
+PK1 = G2 * sk1     -- signature verifier
 
 sk2 = INT.random()
 ck2 = INT.random()
-pk2 = G2 * sk2
+PK2 = G2 * sk2
 
 -- issuer sign ZK credentials
-l1 = ZK.prepare_blind_sign(ck1*G1, ck1)
-st1 = ZK.blind_sign(issuer.sign, l1)
-cred1 = ZK.aggregate_creds(ck1, {st1})
+Lambda1 = ZK.prepare_blind_sign(ck1*G1, ck1) -- credential request:       p -> i
+SigmaTilde1 = ZK.blind_sign(issuer.sign, Lambda1)    -- issuer signs credential:  i -> p
+Sigma1 = ZK.aggregate_creds(ck1, {SigmaTilde1})  -- credential sigma          p -> store
 
-l2 = ZK.prepare_blind_sign(ck2*G1, ck2)
-st2 = ZK.blind_sign(issuer.sign, l2)
-cred2 = ZK.aggregate_creds(ck2, {st2})
+Lambda2 = ZK.prepare_blind_sign(ck2*G1, ck2)
+SigmaTilde2 = ZK.blind_sign(issuer.sign, Lambda2)
+Sigma2 = ZK.aggregate_creds(ck2, {SigmaTilde2})
 
 -- sign: σ = δ * ( H(msg)*G1 )
 
-hm = ECP.hashtopoint(msg) -- message hash
+UID = ECP.hashtopoint(msg) -- the message's hash is the unique identifier
+
+--------------
+-- SETUP done
+--------------
 
 print "--------------------------"
 print "first base signing session"
 r = INT.random()
-sm = hm * r
+R = UID * r      -- session
 
--- add public keys to session
-pm = G2 * r
-pm = pm + pk1 + pk2
+-- add public keys to public session key
+PM = (G2 * r) + PK1 + PK2
+
+-- Session opener broadcasts:
+-- 1. R   - base G1 point for signature session
+-- 2. PM  - base G2 point for public multi-signature key
+-- 3. msg - the message to be signed
 
 -- proofs of valid signature
 -- uses public session key as UID
-p1,z1 = ZK.prove_cred_uid(issuer.verify, cred1, ck1, hm)
-p2,z2 = ZK.prove_cred_uid(issuer.verify, cred2, ck2, hm)
+Proof1,z1 = ZK.prove_cred_uid(issuer.verify, Sigma1, ck1, UID)
+Proof2,z2 = ZK.prove_cred_uid(issuer.verify, Sigma2, ck2, UID)
+-- each signer signs
+S1 = UID * sk1
+S2 = UID * sk2
 
--- add signatures
-sm = sm + ( hm * sk1 )
-sm = sm + ( hm * sk2 ) -- sum of G1
+-- generate the signature
+-- each signer will communicate: UID * sk
+SM = R + S1 + S2
 
-I.print({pub = pm, -- session public keys
-		 sign = sm,
-		 uid = hm,
-		 proofhash1 = sha256( ZEN.serialize( p1 ) ),
-		 proofhash2 = sha256( ZEN.serialize( p2 ) ),
+
+-- print signature contents to screen
+I.print({pub = PM, -- session public keys
+		 sign = SM,
+		 uid = UID,
+		 proofhash1 = sha256( ZEN.serialize( Proof1 ) ),
+		 proofhash2 = sha256( ZEN.serialize( Proof2 ) ),
 		 zeta1 = z1,
 		 zeta2 = z2,
 		 issuer = issuer.verify
 })
 
 -- verify: ε(γ,H(msg)) == ε(G2,σ)
-assert( ZK.verify_cred_uid(issuer.verify, p1, z1, hm),
+assert( ZK.verify_cred_uid(issuer.verify, Proof1, z1, UID),
 		"first proof verification fails")
-assert( ZK.verify_cred_uid(issuer.verify, p2, z2, hm),
+assert( ZK.verify_cred_uid(issuer.verify, Proof2, z2, UID),
 		"second proof verification fails")
-assert( ECP2.miller(pm, hm)
-		   == ECP2.miller(G2, sm),
-        "Signature doesn't validates")
-
-
-print "---------------------------"
-print "second base signing session"
-
-r = INT.random()
-sm = hm * r
-
--- add public keys to session
-pm = G2 * r
-pm = pm + pk1 + pk2
-
--- proofs of valid signature
--- uses public session key as UID
-p1,z1 = ZK.prove_cred_uid(issuer.verify, cred1, ck1, hm)
-p2,z2 = ZK.prove_cred_uid(issuer.verify, cred2, ck2, hm)
-
--- add signatures
-sm = sm + ( hm * sk1 )
-sm = sm + ( hm * sk2 ) -- sum of G1
-
-I.print({pub = pm, -- session public keys
-		 sign = sm,
-		 uid = hm,
-		 proofhash1 = sha256( ZEN.serialize( p1 ) ),
-		 proofhash2 = sha256( ZEN.serialize( p2 ) ),
-		 zeta1 = z1,
-		 zeta2 = z2,
-		 issuer = issuer.verify
-})
-
--- verify: ε(γ,H(msg)) == ε(G2,σ)
-assert( ZK.verify_cred_uid(issuer.verify, p1, z1, hm),
-		"first proof verification fails")
-assert( ZK.verify_cred_uid(issuer.verify, p2, z2, hm),
-		"second proof verification fails")
-assert( ECP2.miller(pm, hm)
-		   == ECP2.miller(G2, sm),
+assert( ECP2.miller(PM, UID)
+		   == ECP2.miller(G2, SM),
         "Signature doesn't validates")
